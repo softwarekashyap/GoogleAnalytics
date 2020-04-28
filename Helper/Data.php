2fcbc39707ac0a1ca9833babfd364c42620f7845
RKs <softwarekashyap@gmail.com>
2020-04-28 23:23:21 +0530

    Initail Update

---
+++ Helper/Data.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @author Kashyap Team
+ * @copyright Copyright (c) 2018 Kashyap (http://kashyapsoftware.com/)
+ * @package Kashyap_GoogleAnalytics
+*/
+
+namespace Kashyap\GoogleAnalytics\Helper;
+
+use Kashyap\Core\Helper\AbstractData;
+
+class Data extends AbstractData
+{
+	const XML_PATH_GENERAL_ENABLED = 'googleanalytics/general/is_enabled';
+	const XML_PATH_GENERAL_MAIN_TRACKING = 'googleanalytics/general/tracking_id';
+	const XML_PATH_GENERAL_SECOND_TRACKING = 'googleanalytics/general/tracking_id2';
+	const XML_PATH_GENERAL_LINK_ATTRIBUTION = 'googleanalytics/general/link_attribution';
+	const XML_PATH_GENERAL_IP_ANONYMIZATION = 'googleanalytics/general/ip_anonymization';
+	const XML_PATH_GENERAL_DEBUG = 'googleanalytics/general/debug';
+
+	public function isEnabled($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_ENABLED, $storeId);
+
+	}
+
+	public function getMainTracking($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_MAIN_TRACKING, $storeId);
+	}
+
+	public function getSecondTracking($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_SECOND_TRACKING, $storeId);
+	}
+
+	public function getLinkAttribution($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_LINK_ATTRIBUTION, $storeId);
+	}
+
+	public function getIpAnonymization($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_IP_ANONYMIZATION, $storeId);
+	}
+
+	public function getDebug($storeId = null)
+	{
+		return $this->getConfigValue(self::XML_PATH_GENERAL_DEBUG, $storeId);
+	}
+}
---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Kashyap Team
+ * @copyright Copyright (c) 2018 Kashyap (http://kashyapsoftware.com/)
+ * @package Kashyap_GoogleAnalytics
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Kashyap_GoogleAnalytics',
+    __DIR__
+);
